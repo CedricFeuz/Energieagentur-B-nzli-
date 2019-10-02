e332e3140f667d5d4c600533d6d243714490c1db
YaraFischer <55987923+YaraFischer@users.noreply.github.com>
2019-10-02 14:30:17 +0200

    Add files via upload

--- Manager.java
+++ Manager.java
@@ -1,10 +1,10 @@
 package Control;
+
 /**
  * @author Yara Fischer
- * @date 01.10.19
+ * @date 02.10.19
  * @Version 1.0
  */
-
 import java.sql.Timestamp;
 import java.util.HashMap;
 
@@ -15,126 +15,133 @@ import Model.ESLListe;
 import Model.ExportDaten;
 import Model.SdatDaten;
 import Model.SdatListe;
+import View.ErfolgGui;
 import View.ExportierenGui;
 import View.J_VerbrauchsGui;
 import View.StartGui;
 import View.T_VerbrauchsGui;
 import View.ZaehlerstandGui;
 
-public class Manager extends JFrame{
-	
-	//Model
+public class Manager extends JFrame {
+
+	// Model
 	private ExportDaten export;
 	private SdatListe sdatEinspeisen;
 	private SdatListe sdatBezogen;
 	private ESLListe eslEinspeisen;
 	private ESLListe eslBezogen;
-	
-	//View
+	// View
 	private JFrame frame;
 	private StartGui startGui;
 	private ZaehlerstandGui zaehlerstandGui;
 	private T_VerbrauchsGui t_VerbrauchsGui;
 	private J_VerbrauchsGui j_VerbrauchsGui;
 	private ExportierenGui exportierenGui;
-	
-	
+	private ErfolgGui erfolgGui;
+
 	public Manager() {
-		//Model
+		// Model
 		sdatEinspeisen = new SdatListe();
 		sdatBezogen = new SdatListe();
 		eslBezogen = new ESLListe();
 		eslEinspeisen = new ESLListe();
-		
-		//View
+
+		// View
+		frame = new JFrame();
 		startGui = new StartGui(frame, this);
 		zaehlerstandGui = new ZaehlerstandGui(frame, this);
 		t_VerbrauchsGui = new T_VerbrauchsGui(frame, this);
 		j_VerbrauchsGui = new J_VerbrauchsGui(frame, this);
 		exportierenGui = new ExportierenGui(frame, this);
+		erfolgGui = new ErfolgGui(frame, this);
 	}
-	
-	//Model
+
+	// Model
 	public HashMap<Timestamp, ESLDaten> getEslEinspeisen() {
-		
+
 		return eslEinspeisen.getListEinspeisen();
 	}
-	
-	public HashMap<Timestamp, ESLDaten> getEslBezogen(){
-		
+
+	public HashMap<Timestamp, ESLDaten> getEslBezogen() {
+
 		return eslBezogen.getListBezogen();
 	}
-	
+
 	public void setEslEinspeisen() {
-		
+
 		eslEinspeisen.setListEinspeisen();
 	}
-	
+
 	public void setEslBezogen() {
-		
+
 		eslBezogen.setListBezogen();
 	}
-	
+
 	public double getEslEinspeissenDurchschnitt() {
-		
+
 		return eslEinspeisen.getDurchschnittEinspeissen(eslEinspeisen.getListEinspeisen());
 	}
-	
+
 	public double getEslBezogenDuchschnitt() {
-		
+
 		return eslBezogen.getDurchschnittBezogen(eslBezogen.getListBezogen());
 	}
-	
-	public HashMap<Timestamp, SdatDaten> getSdatEinspeisen(){
-		
+
+	public HashMap<Timestamp, SdatDaten> getSdatEinspeisen() {
+
 		return sdatEinspeisen.getEinspeisen();
 	}
-	
-	public HashMap<Timestamp, SdatDaten> getSdatBezogen(){
-		
+
+	public HashMap<Timestamp, SdatDaten> getSdatBezogen() {
+
 		return sdatBezogen.getBezogen();
 	}
-	
+
 	public void setSdatEinspeisen() {
-		
+
 		sdatEinspeisen.setListEinspeisen();
 	}
-	
+
 	public void setSdatBezogen() {
-	
+
 		sdatBezogen.setListBezogen();
 	}
-	
+
 	public double getSdatEinspeisenDurchschnitt() {
-		
+
 		return sdatEinspeisen.getDurchschnittEinspeissen(sdatEinspeisen.getEinspeisen());
 	}
-	
+
 	public double getSdatBezogenDuchschnitt() {
-		
+
 		return sdatBezogen.getDurchschnittBezogen(sdatBezogen.getBezogen());
 	}
-	
-	//View
+	public void exportEinspeisen(ESLListe daten) {
+		//export.exportEinspeisen(daten);
+	}
+	public void exportBezogen(ESLListe daten) {
+		//export.exportBezogen(daten);
+	}
+
+	// View
 	public void openStartGui() {
 		startGui.startGui();
 	}
-	
+
 	public void openZaehlerstandGui() {
 		zaehlerstandGui.zaehlerstandGui();
 	}
-	
+
 	public void openT_VerbrauchsGui() {
 		t_VerbrauchsGui.t_VerbrauchsGui();
 	}
+
 	public void openJ_VerbrauchsGui() {
 		j_VerbrauchsGui.j_VerbrauchsGui();
 	}
-	
+
 	public void openExportierenGui() {
 		exportierenGui.exportierenGui();
 	}
-	
-	
 
 }
